88e5d065b4ea6be5497cea781137642d53a89b83
chen9t <chen9t@gmail.com>
2014-07-15 09:30:02 +0800

    delete .pyc

--- access_standards/models.py
+++ access_standards/models.py
@@ -3,10 +3,10 @@ from django.db import models
 
 
 class AccessStandard(models.Model):
-    city = models.CharField(max_length=40, unique=True, verbose_name='城市')
-    emission_standard = models.CharField(max_length=20, null=True, verbose_name='排放标准')
-    standard_details = models.CharField(max_length=100, null=True, verbose_name='标准细节')
-    DVM = models.CharField(max_length=20, null=True, verbose_name='车 管 所')
+    city = models.CharField(max_length=40, unique=True, verbose_name=u'城市')
+    emission_standard = models.CharField(max_length=20, null=True, verbose_name=u'排放标准')
+    standard_details = models.CharField(max_length=100, null=True, verbose_name=u'标准细节')
+    DVM = models.CharField(max_length=20, null=True, verbose_name=u'车 管 所')
 
     def __unicode__(self):
         return self.city
--- access_standards/urls.py
+++ access_standards/urls.py
@@ -6,6 +6,6 @@ from access_standards.models import AccessStandard
 
 urlpatterns = patterns('',
     url(r'^about/$', TemplateView.as_view(template_name='about.html')),
-    url(r'^standard/$', ListView.as_view(model=AccessStandard, context_object_name='standard_list')),
+    url(r'^standard/$', ListView.as_view(model=AccessStandard, context_object_name='standard_list', template_name='standard_list.html')),
     url(r'^$', 'access_standards.views.index', name='index'),
 )
